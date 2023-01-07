d5893a4dc7e06c0fedc972ea0f824e6f7855ea94
Nicholas Lee <103685816+BickBee@users.noreply.github.com>
2023-01-06 22:08:39 -0500

    Added Hard Bot

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const images = document.getElementsByTagName("img");
 let moves = [" "," "," "," "," "," "," "," "," "];
 let current_move = "X";
-let play_bot = 1;
+let play_bot = 2;
 let first_move = Math.floor(Math.random() * 2) // Chooses random number of either 0 or 1
 
 function change_board() {
@@ -10,6 +10,13 @@ function change_board() {
         botMoveEasy();
     } else if ((first_move == 1) && (play_bot = 2)) {
         current_move = "O";
+        
+        // ======================
+        // let movesCopy = [...moves];
+        // movesCopy[0] = "P";
+        // console.log(movesCopy);
+        // console.log(moves);
+        // ======================
         botMoveHard();
     }
 
@@ -43,6 +50,65 @@ function change_board() {
     }
 }
 
+//=================================================================================================
+// Returns "score" of movesCopy (Bot is always O)
+function returnBestMove(movesCopy, turn) {
+    // Base Cases
+    if ((detectWin2(movesCopy)) && (turn == 0)) {
+        return -1;
+    } else if ((detectWin2(movesCopy)) && (turn == 1)) {
+        return 1;
+    } else if (detectTie2(movesCopy)) {
+        return 0;
+    }
+
+    if (turn === 0) {
+        let current_max = -1000;
+        for (let i = 0; i < movesCopy.length; ++i) {
+            if (movesCopy[i] === " ") {
+                let movesCopy2 = [...movesCopy];
+                movesCopy2[i] = "O";
+                current_max = Math.max(current_max, returnBestMove(movesCopy2, 1));
+            }
+        }
+        return current_max;
+    } else if (turn === 1) {
+        let current_min = 1000;
+        for (let i = 0; i < movesCopy.length; ++i) {
+            if (movesCopy[i] === " ") {
+                let movesCopy2 = [...movesCopy];
+                movesCopy2[i] = "X";
+                current_min = Math.min(current_min, returnBestMove(movesCopy2, 0));
+            }
+        }
+        return current_min;
+    }
+}
+
+function botMoveHard() {
+    let best_score = -10000;
+    let best_index;
+
+    for (let i = 0; i < moves.length; ++i) {
+        if (moves[i] === " ") {
+            let movesCopy = [...moves];
+            movesCopy[i] = "O";
+            let temp_score = returnBestMove(movesCopy, 1);
+            console.log(temp_score);
+            if (temp_score > best_score) {
+                best_score = temp_score;
+                best_index = i;
+            }
+        }
+    }
+
+    // Change moves[index of move] to a O and change the current move to "X"
+    images[best_index].src = "/images/Circle.png";
+    moves[best_index] = current_move;
+    current_move = "X";
+}
+//=================================================================================================
+
 function detectEOG() {
     // If Someone Wins
     if (detectWin()) {
@@ -63,15 +129,8 @@ function detectEOG() {
     }
 }
 
-//=================================================================================================
-function botMoveHard() {
-    // Dont Forget to change moves[index of move] to a O
-    // And change the current move to "X"
-}
-//=================================================================================================
-
 function botMoveEasy() {
-    var unused_squares = [];
+    let unused_squares = [];
     for (let i = 0; i < moves.length; ++i) {
         if (moves[i] === " ") {
             unused_squares.push(i);
@@ -101,6 +160,31 @@ function detectWin() {
 function detectTie() {
     for (let i = 0; i < moves.length; ++i) {
         if (moves[i] == " ") {
+            return false;
+        }
+    }
+    return true;
+}
+
+// Copy
+function detectWin2(copy) {
+    if (((copy[0] !== " ") && (copy[0] == copy[1]) && (copy[1] == copy[2]) && (copy[0] == copy[2])) ||
+        ((copy[3] !== " ") && (copy[3] == copy[4]) && (copy[4] == copy[5]) && (copy[3] == copy[5])) ||
+        ((copy[6] !== " ") && (copy[6] == copy[7]) && (copy[7] == copy[8]) && (copy[6] == copy[8])) ||
+        ((copy[0] !== " ") && (copy[0] == copy[3]) && (copy[3] == copy[6]) && (copy[0] == copy[6])) ||
+        ((copy[1] !== " ") && (copy[1] == copy[4]) && (copy[4] == copy[7]) && (copy[1] == copy[7])) ||
+        ((copy[2] !== " ") && (copy[2] == copy[5]) && (copy[5] == copy[8]) && (copy[2] == copy[8])) ||
+        ((copy[0] !== " ") && (copy[0] == copy[4]) && (copy[4] == copy[8]) && (copy[0] == copy[8])) ||
+        ((copy[2] !== " ") && (copy[2] == copy[4]) && (copy[4] == copy[6]) && (copy[2] == copy[6])))
+        {
+            return true;
+        }
+        return false;
+}
+
+function detectTie2(copy) {
+    for (let i = 0; i < copy.length; ++i) {
+        if (copy[i] == " ") {
             return false;
         }
     }
